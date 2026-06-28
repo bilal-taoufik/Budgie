9920c30a4571d5803029a4cc7ecdec6809449f52
bilal-taoufik <bll.taoufik@gmail.com>
2026-06-28 15:00:17 +0200

    feat: backoffice

--- src/app/Http/Controllers/Admin/UserController.php
+++ src/app/Http/Controllers/Admin/UserController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\UserRequest;
+use App\Mail\VerifyMail;
+use App\Mail\WelcomeMail;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\View\View;
 
 class UserController extends Controller
@@ -22,17 +25,26 @@ class UserController extends Controller
     {
         $validated = $request->validated();
 
-        User::create([
+        $user = User::create([
             'firstname' => $validated['firstname'],
             'lastname' => $validated['lastname'],
             'email' => $validated['email'],
             'password' => Hash::make($validated['password']),
             'role' => 'admin',
-            'email_verified' => true,
+            'email_verified' => false,
+            'email_verification_token' => bin2hex(random_bytes(32)),
+            'email_verification_expires_at' => now()->addHours(24),
         ]);
 
+        $verificationUrl = route('verify.email', [
+            'token' => $user->email_verification_token,
+        ]);
+
+        Mail::to($user->email)->send(new WelcomeMail($user));
+        Mail::to($user->email)->send(new VerifyMail($user, $verificationUrl));
+
         return redirect()->route('admin.users.index')
-            ->with('success', 'Admin ajoute avec succes.');
+            ->with('success', 'Admin ajoute avec succes. Un e-mail de verification lui a ete envoye.');
     }
 
     public function delete(User $user): RedirectResponse
@@ -53,5 +65,3 @@ class UserController extends Controller
             ->with('success', 'Utilisateur supprime avec succes.');
     }
 }
-
-
